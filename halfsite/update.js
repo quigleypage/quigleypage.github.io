9ed665ad29cea0db79d39f56022b67fe6e0c4839
Kevin Quigley <kquigley27@gmail.com>
2020-09-18 20:39:11 -0400

    pushing halfsite v1

---
+++ halfsite/update.js
@@ -0,0 +1,6 @@
+function updateSite(){
+    
+    document.getElementById("site").innerHTML = document.getElementById("HTML").value;
+    document.getElementById("siteStyle").innerHTML = document.getElementById("CSS").value;
+    document.getElementById("siteScript").innerHTML = document.getElementById("JS").value;
+}
