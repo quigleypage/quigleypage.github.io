ba7dabbb532c251dfadfa40c0033c29e7acbc07c
Kevin Quigley <kquigley27@gmail.com>
2020-10-11 21:43:59 -0400

    Push updates to halfsite

--- halfsite/update.js
+++ halfsite/update.js
@@ -1,6 +1,35 @@
 function updateSite(){
-    
     document.getElementById("site").innerHTML = document.getElementById("HTML").value;
     document.getElementById("siteStyle").innerHTML = document.getElementById("CSS").value;
     document.getElementById("siteScript").innerHTML = document.getElementById("JS").value;
+}
+
+function showHTML(){
+    document.getElementById("div-HTML").style.gridColumn = "1/12";
+    document.getElementById("div-HTML").style.display = "grid";
+    document.getElementById("div-CSS").style.display = "none";
+    document.getElementById("div-JS").style.display = "none";
+}
+
+function showCSS(){
+    document.getElementById("div-CSS").style.gridColumn = "1/12";
+    document.getElementById("div-CSS").style.display = "grid";
+    document.getElementById("div-HTML").style.display = "none";
+    document.getElementById("div-JS").style.display = "none";
+}
+
+function showJS(){
+    document.getElementById("div-JS").style.gridColumn = "1/12";
+    document.getElementById("div-JS").style.display = "grid";
+    document.getElementById("div-CSS").style.display = "none";
+    document.getElementById("div-HTML").style.display = "none";
+}
+
+function showAll(){
+    document.getElementById("div-HTML").style.gridColumn = "1/5";
+    document.getElementById("div-CSS").style.gridColumn = "5/8";
+    document.getElementById("div-JS").style.gridColumn = "8/12";
+    document.getElementById("div-JS").style.display = "grid";
+    document.getElementById("div-CSS").style.display = "grid";
+    document.getElementById("div-HTML").style.display = "grid";
 }
