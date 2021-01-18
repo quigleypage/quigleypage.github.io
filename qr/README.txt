176a427fbb281640486d38ff47dc349d5e96a918
Kevin Quigley <kquigley27@gmail.com>
2021-01-17 21:58:16 -0500

    qr scanner v1

---
+++ qr/README.txt
@@ -0,0 +1,2 @@
+A simple QR code reading app by Kevin Quigley
+Based heavily on the library designed by mebjas: https://github.com/mebjas/html5-qrcode
---
+++ qr/script.js
@@ -0,0 +1,14 @@
+function onScanSuccess(qrMessage) {
+	// handle the scanned code as you like
+  console.log(`QR matched = ${qrMessage}`);
+  document.getElementById("result").innerHTML = "<br><a href='" + qrMessage + "' target='_blank'>" + qrMessage + "</a>";
+}
+
+function onScanFailure(error) {
+	// handle scan failure, usually better to ignore and keep scanning
+	console.warn(`QR error = ${error}`);
+}
+
+let html5QrcodeScanner = new Html5QrcodeScanner(
+	"reader", { fps: 10, qrbox: 250 }, /* verbose= */ true);
+html5QrcodeScanner.render(onScanSuccess, onScanFailure);
