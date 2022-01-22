38aa595122c8edcede000b230a8abc3abb3b6fa4
Kevin Quigley <kquigley27@gmail.com>
2022-01-21 22:28:17 -0500

    use roboto font and add instructions

--- grid-test/grid.js
+++ grid-test/grid.js
@@ -128,3 +128,19 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+
+var modal2 = document.getElementById("myModal2");
+var btn2 = document.getElementById("myBtn2");
+var span2 = document.getElementsByClassName("close")[1];
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+}
