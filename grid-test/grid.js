d5b2eaea6f073cc79bdedf26be1319c73421e4e8
Kevin Quigley <kquigley27@gmail.com>
2022-01-21 22:36:25 -0500

    fix modal closure bug

--- grid-test/grid.js
+++ grid-test/grid.js
@@ -127,6 +127,9 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
 }
 
 
@@ -138,9 +141,4 @@ btn2.onclick = function() {
 }
 span2.onclick = function() {
   modal2.style.display = "none";
-}
-window.onclick = function(event) {
-  if (event.target == modal2) {
-    modal2.style.display = "none";
-  }
 }
