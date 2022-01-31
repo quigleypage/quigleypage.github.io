8a377aae748ac2de3f273d13862a6340456af9cc
Kevin Quigley <kquigley27@gmail.com>
2022-01-30 20:30:05 -0500

    get cookie after setting
    attempting to fix issue where saving only works after page refresh

--- quiggle/grid.js
+++ quiggle/grid.js
@@ -330,6 +330,7 @@ function setCookie(cname, cvalue) {
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     let expires = "expires="+ d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    getCookie(cname);
 }
 
 function getCookie(cname) {
