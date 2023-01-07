5c43cc135eaa3cac83052b40be87b434589685c6
Kevin Quigley <kquigley27@gmail.com>
2023-01-06 21:15:21 -0500

    add stores

--- scan/scan.js
+++ scan/scan.js
@@ -28,6 +28,11 @@ window.addEventListener('load', function () {
               console.log(result)
               document.getElementById('result').textContent = result.text
               document.getElementById('targetLink').innerHTML = "<a href='https://www.target.com/s?searchTerm=" + result.text + "' target='_blank'>Target</a>";
+              document.getElementById('googleLink').innerHTML = "<a href='https://www.google.com/search?q=" + result.text + "&tbm=shop' target='_blank'>Google</a>";
+              document.getElementById('walmartLink').innerHTML = "<a href='https://www.walmart.com/search?q=" + result.text + "' target='_blank'>Walmart</a>";
+              document.getElementById('amazonLink').innerHTML = "<a href='https://www.amazon.com/s?k=" + result.text + "' target='_blank'>Amazon</a>";
+              document.getElementById('hobbyLink').innerHTML = "<a href='https://www.hobbylobby.com/search/?text=" + result.text + "' target='_blank'>Hobby Lobby</a>";
+              document.getElementById('michaelsLink').innerHTML = "<a href='https://www.michaels.com/search?q=" + result.text + "' target='_blank'>Michaels</a>";
             }
             if (err && !(err instanceof ZXing.NotFoundException)) {
               console.error(err)
@@ -40,7 +45,12 @@ window.addEventListener('load', function () {
         document.getElementById('resetButton').addEventListener('click', () => {
           codeReader.reset()
           document.getElementById('result').textContent = '';
+          document.getElementById('googleLink').innerHTML = "";
+          document.getElementById('amazonLink').innerHTML = "";
           document.getElementById('targetLink').innerHTML = "";
+          document.getElementById('walmartLink').innerHTML = "";
+          document.getElementById('michaelsLink').innerHTML = "";
+          document.getElementById('hobbyLink').innerHTML = "";
           console.log('Reset.')
         })
 
