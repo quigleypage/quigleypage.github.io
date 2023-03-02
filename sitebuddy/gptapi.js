87bb631a8672f5942caccb3a92b5f65dd5a37315
Kevin Quigley <kquigley27@gmail.com>
2023-03-01 20:40:53 -0500

    optimize implementation for gpt3.5

--- sitebuddy/gptapi.js
+++ sitebuddy/gptapi.js
@@ -1,4 +1,4 @@
-headerVerbiage = "header (may include a banner, navbar, or search box, as appropriate)";
+/*headerVerbiage = "header (may include a banner, navbar, or search box, as appropriate)";
 bodyVerbiage = "body content (without headers or footers)";
 footerVerbiage = "footer";
 
@@ -6,8 +6,7 @@ async function generateText(descPrompt, tag) {
     if(tag == headerVerbiage){
         document.getElementById("loading-wheel").style.display = "block";
     }
-    //fullPrompt = "Generate HTML, including style tags, script tags (if appropriate), but not img tags, for the homepage " + tag + " of the below described website:\n\n" + descPrompt;
-    fullPrompt = "Generate HTML, including style tags, script tags (if appropriate), but not img tags, for the homepage of the below described website:\n\n" + descPrompt;
+    fullPrompt = "Generate HTML, including style tags, script tags (if appropriate), but not img tags, for the homepage " + tag + " of the below described website:\n\n" + descPrompt;
     try {
         encodedPrompt = { promptText: fullPrompt };
         const response = await axios.post('https://gpt-test-app.herokuapp.com/generate-text', encodedPrompt, {
@@ -17,16 +16,36 @@ async function generateText(descPrompt, tag) {
         });
         console.log(response.data.text);
         document.getElementById(tag).innerHTML = response.data.text;
-        /*if(tag == headerVerbiage){
+        if(tag == headerVerbiage){
             generateText(descPrompt, footerVerbiage);
         }
         else if(tag == footerVerbiage){
             generateText(descPrompt, bodyVerbiage);
         }
-        else{*/
+        else{
             document.getElementById("initialSite").innerHTML = "";
-        //}
+        }
 
+    } catch (error) {
+        console.error(error);
+        document.getElementById(tag).innerHTML = "Error: " + error;
+    }
+
+}*/
+
+async function generateText(descPrompt) {
+    document.getElementById("loading-wheel").style.display = "block";
+    fullPrompt = "Generate HTML, including style tags, script tags (if appropriate), but not img tags, for the homepage of the below described website:\n\n" + descPrompt;
+    try {
+        encodedPrompt = { promptText: fullPrompt };
+        const response = await axios.post('https://gpt-test-app.herokuapp.com/generate-text', encodedPrompt, {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+        });
+        console.log(response.data.text);
+        document.getElementById("initialSite").innerHTML = "";
+        document.getElementById("newSite").innerHTML = response.data.text;
     } catch (error) {
         console.error(error);
         document.getElementById(tag).innerHTML = "Error: " + error;
