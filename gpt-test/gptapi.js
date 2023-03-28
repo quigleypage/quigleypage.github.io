c842abd88082cdf6b386792cbbe21c990ba4195a
Kevin Quigley <kquigley27@gmail.com>
2023-03-27 21:21:19 -0400

    update gpt-test to handle multiple models

--- gpt-test/gptapi.js
+++ gpt-test/gptapi.js
@@ -3,15 +3,33 @@ messageArray = [];
 async function generateText(prompt) {
     document.getElementById("AIResponse").innerHTML = "Loading...";
     messageArray.push({role: "user", content: prompt})
+
+    encodedMessageArray = { promptText: messageArray, version: 3 };
+    isImage = false;
+
+    if(document.getElementById("GPT4").checked){
+        encodedMessageArray = { promptText: messageArray, version: 4 };
+    }
+    else if(document.getElementById("DALLE").checked){
+        encodedMessageArray = { promptText: prompt, version: -1 };
+        isImage = true;
+    }
+
     try {
-        encodedMessageArray = { promptText: messageArray };
         const response = await axios.post('https://gpt-test-app.herokuapp.com/generate-text', encodedMessageArray, {
             headers: {
               'Content-Type': 'application/json'
             }
         });
         console.log(response.data.text);
-        document.getElementById("AIResponse").innerHTML = response.data.text;
+        if(!isImage){
+            document.getElementById("AIResponse").innerHTML = response.data.text;
+            document.getElementById("DALLEResponse").src = "";
+        }
+        else{
+            document.getElementById("AIResponse").innerHTML = "";
+            document.getElementById("DALLEResponse").src = response.data.text;
+        }
     } catch (error) {
         console.error(error);
         document.getElementById("AIResponse").innerHTML = "Error: " + error;
