a2c72bd1b2de71538b20dcf343637f2e1f150cc6
Kevin Quigley <kquigley27@gmail.com>
2023-04-09 17:59:37 -0400

    cosmetic updates

--- mockup/gptapi.js
+++ mockup/gptapi.js
@@ -2,11 +2,11 @@ messageArray = [];
 
 async function generateText(prompt) {
     document.getElementById('userInput').value = "";
-    document.getElementById("AIResponse").innerHTML += '<div class="user-message-card"><div class="sender-name">You</div><div class="message">' + prompt + '</div></div>';
+    //document.getElementById("AIResponse").innerHTML += '<div class="user-message-card"><div class="sender-name">You</div><div class="message">' + prompt + '</div></div>';
+    document.getElementById("AIResponse").innerHTML += '<div class="user-message-card"><div class="message">' + prompt + '</div></div>';
     document.getElementById("AIResponse").scrollTop = document.getElementById("AIResponse").scrollHeight;
     if(messageArray.length == 0){
-        messageArray.push({role: "user", content: "Generate HTML code, including style tags, for the below described component. In your response, only provide the code. Do not include any commentary or explanations of the code.\n\n" + prompt})
-        document.getElementById("canvasParent").style.display = "block";
+        messageArray.push({role: "user", content: "Generate HTML code, including style tags, for the below described component. In your response, only provide the code. Do not include any commentary or explanations of the code.\n\n" + prompt});
         document.getElementById("userInput").placeholder = "Describe desired adjustments...";
     }
     else{
@@ -26,6 +26,7 @@ async function generateText(prompt) {
         
         messageArray.push({role: "assistant", content: botResponse})
         document.getElementById("botCanvas").innerHTML = botResponse;
+        document.getElementById("canvasParent").style.display = "block";
         document.getElementById("AIResponse").scrollTop = 0;
     } catch (error) {
         console.error(error);
