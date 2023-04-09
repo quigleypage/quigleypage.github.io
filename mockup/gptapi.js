cccfe76f4bd044e6300186f3adcbeb00c1c87eaa
Kevin Quigley <kquigley27@gmail.com>
2023-04-09 15:00:46 -0400

    push mock-up v1

---
+++ mockup/gptapi.js
@@ -0,0 +1,33 @@
+messageArray = [];
+
+async function generateText(prompt) {
+    document.getElementById('userInput').value = "";
+    document.getElementById("AIResponse").innerHTML += '<div class="user-message-card"><div class="sender-name">You</div><div class="message">' + prompt + '</div></div>';
+    document.getElementById("AIResponse").scrollTop = document.getElementById("AIResponse").scrollHeight;
+    if(messageArray.length == 0){
+        messageArray.push({role: "user", content: "Generate HTML code, including style tags, for the below described component. In your response, only provide the code. Do not include any commentary or explanations of the code.\n\n" + prompt})
+        document.getElementById("userInput").placeholder = "Describe desired adjustments...";
+    }
+    else{
+        messageArray.push({role: "user", content: prompt});
+    }
+    try {
+        encodedMessageArray = { promptText: messageArray, version: 3 };
+        const response = await axios.post('https://gpt-test-app.herokuapp.com/generate-text', encodedMessageArray, {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+        });
+        botResponse = response.data.text;
+        console.log(botResponse);
+        botResponse = botResponse.replaceAll("```", "");
+        console.log(botResponse);
+        
+        messageArray.push({role: "assistant", content: botResponse})
+        document.getElementById("AIResponse").innerHTML += '<div class="bot-message-card"><div class="sender-name">Bot</div><div class="message">' + botResponse + '</div></div>';
+        document.getElementById("AIResponse").scrollTop = document.getElementById("AIResponse").scrollHeight;
+    } catch (error) {
+        console.error(error);
+        document.getElementById("AIResponse").innerHTML = "Error: " + error;
+    }
+}
