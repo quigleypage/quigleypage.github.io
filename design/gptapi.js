9e6400d2fe80b68f52beeaa3c45b4a33b7d8b32a
Kevin Quigley <kquigley27@gmail.com>
2024-01-22 14:01:45 -0500

    minor style tweaks

--- design/gptapi.js
+++ design/gptapi.js
@@ -37,23 +37,6 @@ async function generateText(prompt) {
             document.getElementById("errorLine").innerHTML = "";
             currentView = htmlArray.length;
 
-            /*
-            //accordion handler needs to activate listener retroactively
-            var acc = document.getElementsByClassName("accordion");
-            var i;
-            
-            for (i = 0; i < acc.length; i++) {
-            acc[i].addEventListener("click", function() {
-                this.classList.toggle("active");
-                var panel = this.nextElementSibling;
-                if (panel.style.maxHeight) {
-                panel.style.maxHeight = null;
-                } else {
-                panel.style.maxHeight = panel.scrollHeight + "px";
-                } 
-            });
-            }*/
-
         } 
         catch (error) {
             console.error(error);
