a56a430c6d12ddea37c30ddfdd017bd179eacd3b
Kevin Quigley <kquigley27@gmail.com>
2024-08-01 21:14:47 -0400

    add bold and italic support

--- mystery/gptapi.js
+++ mystery/gptapi.js
@@ -48,6 +48,15 @@ async function generateText(prompt) {
 
         messageArray.push({role: "assistant", content: botResponse})
         botResponseCleaned = botResponse.replace(/\n/g, "<br />");
+        // Bold
+        botResponseCleaned = botResponseCleaned.replaceAll(/\*\*(.*?)\*\*/g, '<strong>$1</strong>');
+        botResponseCleaned = botResponseCleaned.replaceAll(/__(.*?)__/g, '<strong>$1</strong>');
+        // Italic
+        botResponseCleaned = botResponseCleaned.replaceAll(/\*(.*?)\*/g, '<em>$1</em>');
+        botResponseCleaned = botResponseCleaned.replaceAll(/_(.*?)_/g, '<em>$1</em>');
+        // Bold and Italic
+        botResponseCleaned = botResponseCleaned.replaceAll(/\*\*\*(.*?)\*\*\*/g, '<strong><em>$1</em></strong>');
+        botResponseCleaned = botResponseCleaned.replaceAll(/___(.*?)___/g, '<strong><em>$1</em></strong>');
         scrollHeightTracker = document.getElementById("AIResponse").scrollHeight;
         document.getElementById("AIResponse").innerHTML += '<div class="bot-message-card"><div class="sender-name">Q</div><div class="message">' + botResponseCleaned + '</div></div>';
         //document.getElementById("AIResponse").scrollTop = document.getElementById("AIResponse").scrollHeight;
