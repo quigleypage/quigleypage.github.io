7e4e4edd9ac1b92090bedebe4c89c7accca845e0
Kevin Quigley <kquigley27@gmail.com>
2024-08-15 10:49:47 -0400

    heavily simplify

--- pagedesigner/qpagedesigner.js
+++ pagedesigner/qpagedesigner.js
@@ -1,18 +1,18 @@
 messageArray = [{role: "system", content: "You are a design assistant that generates HTML code, including style and script tags, for the user's described web page. Wrap any CSS code in style tags, and JS code in script tags, within the HTML. Define custom classes instead of using generic tags that may conflict with existing page elements.  In your responses, only provide code. Do not include any introductory commentary, code explanations, or elaborations at any point in the conversation."}];
-model = 7;
-currentCode = "";
+qmodel = 7;
+qcurrentCode = "";
 
 async function qgenerateText(prompt) {
     if(prompt != ""){
-        document.getElementById('userInput').value = "";
-        document.getElementById('userInput').disabled = true;
-        document.getElementById('sendButton').disabled = true;
-        document.getElementById('sendButton').innerHTML = '<div id="spinner" class="q-lds-default"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>';
-        document.getElementById("userInput").placeholder = "One moment please..."
+        document.getElementById('quserInput').value = "";
+        document.getElementById('quserInput').disabled = true;
+        document.getElementById('qsendButton').disabled = true;
+        document.getElementById('qsendButton').innerHTML = '<div id="spinner" class="q-lds-default"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>';
+        document.getElementById("quserInput").placeholder = "One moment please..."
 
         messageArray.push({role: "user", content: prompt});
         try {
-            encodedMessageArray = { promptText: messageArray, version: model };
+            encodedMessageArray = { promptText: messageArray, version: qmodel };
             const response = await axios.post('https://gpt-test-app.herokuapp.com/generate-text', encodedMessageArray, {
                 headers: {
                 'Content-Type': 'application/json'
@@ -27,17 +27,17 @@ async function qgenerateText(prompt) {
             console.log(botResponse);
             
             messageArray.push({role: "assistant", content: botResponse});
-            document.getElementById("AIResponse").innerHTML = botResponse;
-            currentCode = botResponse;
-            document.getElementById("userInput").placeholder = "Describe desired adjustments";
+            document.getElementById("q-AIResponse").innerHTML = botResponse;
+            qcurrentCode = botResponse;
+            document.getElementById("quserInput").placeholder = "Describe desired adjustments";
 
         } 
         catch (error) {
             console.error(error);
         }
-        document.getElementById('sendButton').innerHTML = '<i class="material-icons">send</i>';
-        document.getElementById('sendButton').disabled = false;
-        document.getElementById('userInput').disabled = false;
+        document.getElementById('qsendButton').innerHTML = '<i class="material-icons">send</i>';
+        document.getElementById('qsendButton').disabled = false;
+        document.getElementById('quserInput').disabled = false;
     }
 }
 
@@ -45,9 +45,9 @@ async function qgenerateText(prompt) {
 
 function qdownloadCode(){
     
-    if(currentCode != ""){
+    if(qcurrentCode != ""){
         document.getElementById("qlogo").style.animation = "q-spin-and-expand 0.8s ease-out 1";
-        const data = currentCode;
+        const data = qcurrentCode;
         const blob = new Blob([data], { type: 'text/html' });
         const url = URL.createObjectURL(blob);
 
