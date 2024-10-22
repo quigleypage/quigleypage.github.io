5477a7d0990388d3d3011932cc39fb779cbfbf3d
Kevin Quigley <kquigley27@gmail.com>
2024-10-21 20:53:10 -0400

    push stroll prototype

--- stroll/stroll.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    for (let i = 0; i < 49; i++) {
-      const gridItem = document.createElement('div');
-      gridItem.className = 'grid-item';
-      document.querySelector('.grid').appendChild(gridItem);
-    }
-  });
